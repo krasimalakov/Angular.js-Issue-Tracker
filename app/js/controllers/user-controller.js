e2c60b4be884f18a648da7f4e5e27003178e74dc
Krasimir Malakov <k_malakov@mail.bg>
2016-04-10 06:40:24 +0300

    Added cancel button to edit profile screen and change password screen

--- app/js/controllers/user-controller.js
+++ app/js/controllers/user-controller.js
@@ -77,13 +77,12 @@ app.controller('UserController.ChangePassword', [
     'notifyService',
     'userService',
     function ($scope, $location, notifyService, userService) {
-
         $scope.changePassword = function (userData) {
             userService.changePassword(userData).then(function (result) {
                 notifyService.showInfo('User change password is successful !');
                 $location.path('/');
             }, function (error) {
-                notifyService.showError('User change password failed!', error);
+                notifyService.showError('User change password failed !', error);
             });
         };
     }]);
