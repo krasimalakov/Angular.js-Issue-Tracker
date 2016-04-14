afd8b5b619120a9e9a4084412ac25056d584bf5f
Krasimir Malakov <k_malakov@mail.bg>
2016-04-14 11:06:54 +0300

    Modify "add project" suggestion for labels

---
+++ app/js/directives/input-suggest-list.js
@@ -0,0 +1,20 @@
+app.directive('inputLabelSuggestList', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function ($scope, element, attr) {
+            element.bind("keyup", function (e) {
+                var input=e.target.value;
+                var selectedWords=input.substring(0, input.lastIndexOf(','));
+                var items=eval('$scope.'+attr.inputLabelSuggestList);
+                items.forEach(function (item) {
+                    var initName=item.Name.split(',').pop();
+                    if (selectedWords.length>0){
+                        item.Name=selectedWords+','+initName;
+                    }else {
+                        item.Name=initName;
+                    }
+                });
+            });
+        }
+    }
+}]);
