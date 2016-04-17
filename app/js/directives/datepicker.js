eeeb8716e54b05150b8fd4ae77c6fd0db0b4364b
Krasimir Malakov <k_malakov@mail.bg>
2016-04-17 21:47:33 +0300

    Fix format datapicker

--- app/js/directives/datepicker.js
+++ app/js/directives/datepicker.js
@@ -1,8 +1,8 @@
 app.directive('datepicker', [function () {
     return {
         restrict: 'A',
-        link: function ($scope, element, attr) {
-            $( "#inputDueDate" ).datepicker();
+        link: function ($scope, element, attrs) {
+            $('#' + attrs.id).datepicker().datepicker('option', 'dateFormat', 'yy-mm-dd');
         }
     }
 }]);
