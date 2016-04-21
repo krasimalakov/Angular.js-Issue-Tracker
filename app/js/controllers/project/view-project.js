e7b65214fae216c03976d4f438dafe78bae3dd13
Krasimir Malakov <k_malakov@mail.bg>
2016-04-21 23:11:08 +0300

    Added functionality to view my or all issues into view project

--- app/js/controllers/project/view-project.js
+++ app/js/controllers/project/view-project.js
@@ -10,16 +10,24 @@ app.controller('ProjectController.ViewProject', [
     'issueService',
     function ($scope, $location, $routeParams, notifyService, userService, projectService, issueService) {
         userService.denyNotLoggedUser();
-
+        var allIssues = [],
+            myIssues=[],
+            issuesToView=[];
         projectService.getProject($routeParams.id).then(function (project) {
             $scope.project = project;
             issueService.getProjectIssues(project.Id).then(function (issues) {
-                $scope.allIssues = issues;
+                allIssues=issues;
+                issues.forEach(function (issue) {
+                    if (issue.Assignee.Id==userService.getCurrentUser().id){
+                        myIssues.push(issue);
+                    }
+                });
                 $scope.paginationIssue = {
                     'startPage': 1,
                     'pageSize': 4,
-                    'maxSize': $scope.allIssues.length
+                    'maxSize': 999
                 };
+                $scope.selectMyIssuesToView();
                 $scope.selectIssuesToView();
             }, function (error) {
                 notifyService.showError('Get issues for project Id=' + project.Id + ' request failed !', error);
@@ -29,7 +37,23 @@ app.controller('ProjectController.ViewProject', [
         });
 
         $scope.selectIssuesToView = function () {
-            $scope.issues = $scope.allIssues.slice(($scope.paginationIssue.startPage - 1) * $scope.paginationIssue.pageSize,
+            $scope.issues = issuesToView.slice(($scope.paginationIssue.startPage - 1) * $scope.paginationIssue.pageSize,
                 $scope.paginationIssue.startPage * $scope.paginationIssue.pageSize);
+        };
+
+        $scope.selectAllIssuesToView= function () {
+            $scope.showAllIssue=true;
+            issuesToView=allIssues;
+            $scope.paginationIssue.startPage=1;
+            $scope.paginationIssue.maxSize=issuesToView.length;
+            $scope.selectIssuesToView();
+        };
+
+        $scope.selectMyIssuesToView= function () {
+            $scope.showAllIssue=false;
+            issuesToView=myIssues;
+            $scope.paginationIssue.startPage=1;
+            $scope.paginationIssue.maxSize=issuesToView.length;
+            $scope.selectIssuesToView();
         };
     }]);
