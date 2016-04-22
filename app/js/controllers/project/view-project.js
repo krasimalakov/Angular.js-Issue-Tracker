23772b8972c7ab623a77e6398c6a705affd628b5
Krasimir Malakov <k_malakov@mail.bg>
2016-04-22 15:07:30 +0300

    Added filter - search in title, to view issues in project page

--- app/js/controllers/project/view-project.js
+++ app/js/controllers/project/view-project.js
@@ -115,6 +115,21 @@ app.controller('ProjectController.ViewProject', [
                 });
             }
 
+            // filter by Search in Title
+            issues = issuesToView;
+            issuesToView = [];
+            console.log($scope.search.issue);
+            if (($scope.search.issue === undefined)||($scope.search.issue.trim() == '')) {
+                issuesToView = issues;
+            } else {
+                issues.forEach(function (issue) {
+                    if (issue.Title.toLowerCase().indexOf($scope.search.issue.toLowerCase())>=0) {
+                        console.log('*');
+                        issuesToView.push(issue);
+                    }
+                });
+            }
+
             // finish - apply
             updateSearchData();
             $scope.paginationIssue.startPage = 1;
